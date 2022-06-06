1550150df0fec70221b2110a2deb7403ac919155
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-06-07 02:48:15 +0300

    return entity in register

--- src/controller/guest.py
+++ src/controller/guest.py
@@ -1,16 +1,23 @@
 import logging
-
 import psycopg2 as ps
-from src.database import PostgresDB
+
+from src.database.database import PostgresDB
+from src.model.tenant import Tenant
 
 
 class GuestController:
     def __init__(self, db: PostgresDB):
         self.__db = db
 
-    def register(self, full_name: str, sex: str, city: str, qualities: str, age: int, solvency: bool):
+    def check_tenant(self, user_id: int):
+        return self.__db.check_tenant(user_id)
+
+    def register_tenant(self, user_id: int, full_name: str, sex: str, city: str,
+                        qualities: str, age: int, solvency: bool):
         try:
-            self.__db.add_tenant(full_name, sex, city, qualities, age, solvency)
+            self.__db.add_tenant(user_id, full_name, sex, city, qualities, age, solvency)
+            return Tenant(user_id, full_name, sex, city, qualities, age, solvency)
         except ps.errors.CheckViolation as e:
             logging.error(e)
             logging.error(f'Invalid params while registration tenant with name \'{full_name}\'')
+            return Tenant()

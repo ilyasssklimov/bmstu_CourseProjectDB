e7f8ab348b5198090823f206d660339d21c98e98
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-06-07 02:51:17 +0300

    change struct of project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from aiogram import executor
-from bot import dispatcher
-from logger import init_logger
+from bot.bot import dispatcher
+from bot.logger import init_logger
+import sys
 
 
 if __name__ == '__main__':
+    sys.path.append('.')
     init_logger()
     executor.start_polling(dispatcher, skip_updates=True)

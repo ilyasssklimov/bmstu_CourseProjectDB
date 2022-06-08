7feff35015f804f4ba7e62a5ce498d5a93aa1a54
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-06-08 20:07:14 +0300

    add landlord methods to controllers

--- src/controller/admin.py
+++ src/controller/admin.py
@@ -3,35 +3,35 @@ import psycopg2 as ps
 
 from src.database.database import PostgresDB
 from src.model.tenant import Tenant
+from src.model.landlord import Landlord
 
 
 class AdminController:
     def __init__(self, db: PostgresDB):
         self.__db = db
 
+    # tenant methods
     def get_tenants(self):
         headers = ['id', 'full_name', 'sex', 'city', 'personal_qualities', 'age', 'solvency']
         tenants = [Tenant(*tenant) for tenant in self.__db.get_tenants()]
         return headers, tenants
 
-    def add_tenant(self, tenant):
+    def add_tenant(self, tenant: Tenant):
         try:
-            add_tenant = Tenant(*self.__db.add_tenant(tenant.id, tenant.full_name, tenant.sex, tenant.city,
-                                                      tenant.qualities, tenant.age, tenant.solvency))
+            add_tenant = Tenant(*self.__db.add_tenant(*tenant.get_params()))
             return add_tenant
         except ps.errors.CheckViolation as e:
             logging.error(e)
-            logging.error(f'Invalid params while registration tenant with name \'{tenant.full_name}\'')
+            logging.error(f'Invalid params while adding tenant with name \'{tenant.full_name}\'')
         except Exception as e:
             logging.error(e)
             logging.error(f'Some error while adding tenant with name \'{tenant.full_name}\'')
 
         return Tenant()
 
-    def update_tenant(self, tenant):
+    def update_tenant(self, tenant: Tenant):
         try:
-            upd_tenant = Tenant(*self.__db.update_tenant(tenant.id, tenant.full_name, tenant.sex, tenant.city,
-                                                         tenant.qualities, tenant.age, tenant.solvency))
+            upd_tenant = Tenant(*self.__db.update_tenant(*tenant.get_params()))
             return upd_tenant
         except ps.errors.CheckViolation as e:
             logging.error(e)
@@ -42,7 +42,7 @@ class AdminController:
 
         return Tenant()
 
-    def delete_tenant(self, tenant_id):
+    def delete_tenant(self, tenant_id: int):
         try:
             del_tenant = Tenant(*self.__db.delete_tenant(tenant_id))
             return del_tenant
@@ -51,3 +51,45 @@ class AdminController:
             logging.error(f'Some error while deleting tenant with id = {tenant_id}')
 
         return Tenant()
+
+    # landlord methods
+    def get_landlords(self):
+        headers = ['id', 'full_name', 'city', 'rating', 'age']
+        landlords = [Landlord(*landlord) for landlord in self.__db.get_landlords()]
+        return headers, landlords
+
+    def delete_landlord(self, landlord_id: int):
+        try:
+            del_landlord = Tenant(*self.__db.delete_landlord(landlord_id))
+            return del_landlord
+        except Exception as e:
+            logging.error(e)
+            logging.error(f'Some error while deleting landlord with id = {landlord_id}')
+
+        return Landlord()
+
+    def add_landlord(self, landlord: Landlord):
+        try:
+            add_landlord = Landlord(*self.__db.add_landlord(*landlord.get_params()))
+            return add_landlord
+        except ps.errors.CheckViolation as e:
+            logging.error(e)
+            logging.error(f'Invalid params while adding landlord with name \'{landlord.full_name}\'')
+        except Exception as e:
+            logging.error(e)
+            logging.error(f'Some error while adding landlord with name \'{landlord.full_name}\'')
+
+        return Landlord()
+
+    def update_landlord(self, landlord: Landlord):
+        try:
+            upd_landlord = Landlord(*self.__db.update_landlord(*landlord.get_params()))
+            return upd_landlord
+        except ps.errors.CheckViolation as e:
+            logging.error(e)
+            logging.error(f'Invalid params while updating landlord with name \'{landlord.full_name}\'')
+        except Exception as e:
+            logging.error(e)
+            logging.error(f'Some error while updating landlord with name \'{landlord.full_name}\'')
+
+        return Landlord()
--- src/controller/guest.py
+++ src/controller/guest.py
@@ -3,14 +3,15 @@ import psycopg2 as ps
 
 from src.database.database import PostgresDB
 from src.model.tenant import Tenant
+from src.model.landlord import Landlord
 
 
 class GuestController:
     def __init__(self, db: PostgresDB):
         self.__db = db
 
-    def check_tenant(self, user_id: int):
-        return self.__db.check_tenant(user_id)
+    def check_tenant(self, tenant_id: int):
+        return self.__db.check_tenant(tenant_id)
 
     def register_tenant(self, user_id: int, full_name: str, sex: str, city: str,
                         qualities: str, age: int, solvency: bool):
@@ -21,3 +22,16 @@ class GuestController:
             logging.error(e)
             logging.error(f'Invalid params while registration tenant with name \'{full_name}\'')
             return Tenant()
+
+    def check_landlord(self, landlord_id: int):
+        return self.__db.check_landlord(landlord_id)
+
+    def register_landlord(self, user_id: int, full_name: str, city: str, age: int):
+        rating = 0.0
+        try:
+            self.__db.add_landlord(user_id, full_name, city, rating, age)
+            return Landlord(user_id, full_name, city, rating, age)
+        except ps.errors.CheckViolation as e:
+            logging.error(e)
+            logging.error(f'Invalid params while registration landlord with name \'{full_name}\'')
+            return Landlord()

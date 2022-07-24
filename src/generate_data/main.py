b142739e809905b9e038724b224564b03c2f9f4f
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-07-25 00:43:20 +0900

    move generation function to main

--- src/generate_data/main.py
+++ src/generate_data/main.py
@@ -3,6 +3,7 @@ from src.database.database import PostgresDB
 from src.database.config import DB_ADMIN_PARAMS
 from src.generate_data.user import GenerateData
 from src.generate_data.flat import ParseFlats
+from src.generate_data.config import MOSCOW_FLATS_URL
 
 
 def main():
@@ -10,6 +11,9 @@ def main():
     generate = GenerateData(db)
     generate.generate_users(Types.TENANT)
     generate.generate_users(Types.LANDLORD)
+
+    parse = ParseFlats(db)
+    parse.add_flats(MOSCOW_FLATS_URL)
 
 
 if __name__ == '__main__':

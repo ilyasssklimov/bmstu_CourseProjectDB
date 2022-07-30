defe4be98a2086aa35287a62bce05ff634a82068
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-07-30 14:22:15 +0900

    add init_logger in admin_panel

--- src/admin.py
+++ src/admin.py
@@ -1,10 +1,14 @@
 from PyQt5 import QtWidgets
 import sys
 from src.admin_panel.mainwindow import MainWindow
+from src.logger.logger import init_logger
+from src.logger.config import TargetType
 
 
 def main():
     sys.path.append('.')
+    init_logger(TargetType.ADMIN)
+
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()

9bbffcf286c25b4079f5a691e50603814f25a337
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-08-08 16:01:53 +0900

    supplement admin controller methods

--- src/controller/admin.py
+++ src/controller/admin.py
@@ -14,6 +14,7 @@ class AdminController:
     def get_tenants(self):
         headers = ['id', 'full_name', 'sex', 'city', 'personal_qualities', 'age', 'solvency']
         tenants = [Tenant(*tenant) for tenant in self.__db.get_tenants()]
+        tenants.sort(key=lambda tenant: tenant.id)
         return headers, tenants
 
     def add_tenant(self, tenant: Tenant):
@@ -54,8 +55,9 @@ class AdminController:
 
     # landlord methods
     def get_landlords(self):
-        headers = ['id', 'full_name', 'city', 'rating', 'age']
+        headers = ['id', 'full_name', 'city', 'rating', 'age', 'phone', 'username']
         landlords = [Landlord(*landlord) for landlord in self.__db.get_landlords()]
+        landlords.sort(key=lambda landlord: landlord.id)
         return headers, landlords
 
     def delete_landlord(self, landlord_id: int):
@@ -94,9 +96,50 @@ class AdminController:
 
         return Landlord()
 
+    def check_landlord(self, landlord_id: int):
+        logging.info(f'Checking landlord with id = {landlord_id}')
+        return self.__db.check_landlord(landlord_id)
+
     # flat methods
-    def get_flats(self):
+    def get_flats(self) -> (list[str], list[Flat]):
         headers = ['id', 'owner_id', 'price', 'rooms', 'square', 'address',
                    'metro', 'floor', 'max_floor', 'description']
         flats = [Flat(*flat) for flat in self.__db.get_flats()]
+        flats.sort(key=lambda flat: flat.id)
         return headers, flats
+
+    def delete_flat(self, flat_id: int) -> Flat:
+        try:
+            del_flat = Flat(*self.__db.delete_flat(flat_id))
+            return del_flat
+        except Exception as e:
+            logging.error(e)
+            logging.error(f'Some error while deleting flat with id = {flat_id}')
+
+        return Flat()
+
+    def add_flat(self, flat: Flat) -> Flat:
+        try:
+            add_flat = Flat(*self.__db.add_flat(*flat.get_params()[1:]))
+            return add_flat
+        except ps.errors.CheckViolation as e:
+            logging.error(e)
+            logging.error(f'Invalid params while adding flat with id = {flat.id}')
+        except Exception as e:
+            logging.error(e)
+            logging.error(f'Some error while adding flat with id = {flat.id}')
+
+        return Flat()
+
+    def update_flat(self, flat: Flat) -> Flat:
+        try:
+            upd_flat = Flat(*self.__db.update_flat(*flat.get_params()))
+            return upd_flat
+        except ps.errors.CheckViolation as e:
+            logging.error(e)
+            logging.error(f'Invalid params while updating flat with id = {flat.id}')
+        except Exception as e:
+            logging.error(e)
+            logging.error(f'Some error while updating flat with id = {flat.id}')
+
+        return Flat()

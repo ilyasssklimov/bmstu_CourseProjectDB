076c8788ac5f44911db22f587e020186130dd36f
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-08-09 14:56:33 +0900

    add delete flat trigger

--- src/query/triggers.sql
+++ src/query/triggers.sql
@@ -1,3 +1,4 @@
+-- trigger to delete flats before deleting landlord
 CREATE OR REPLACE FUNCTION public.delete_tenant_flats ()
 RETURNS TRIGGER AS
 $$
@@ -15,3 +16,23 @@ CREATE TRIGGER delete_landlord
 BEFORE DELETE on public.landlord
 FOR EACH ROW
 EXECUTE PROCEDURE public.delete_tenant_flats();
+
+
+-- trigger to delete flats photos before deleting flat
+CREATE OR REPLACE FUNCTION public.delete_flat_photos ()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    DELETE FROM public.flat_photo
+    WHERE flat_id = old.id;
+    return old;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS delete_flat on public.flat;
+
+CREATE TRIGGER delete_flat
+BEFORE DELETE on public.flat
+FOR EACH ROW
+EXECUTE PROCEDURE public.delete_flat_photos();

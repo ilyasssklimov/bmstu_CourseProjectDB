bc22d6d5c29f14984e766dfd2f5cb91aac258bbd
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-08-15 23:29:26 +0900

    add __eq__ to entity type

--- src/bot/config.py
+++ src/bot/config.py
@@ -7,6 +7,18 @@ class EntityType(Enum):
     TENANT = 1
     LANDLORD = 2
     FLAT = 3
+    NEIGHBORHOOD = 4
+
+    def __eq__(self, other):
+        if self.__class__ is other.__class__ and self.value == other.value:
+            return True
+        return False
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return hash(self.value)
 
 
 API_TOKEN = os.getenv('HOSTEL_TOKEN_API')

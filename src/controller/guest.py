e1119f80cc39c23c07f3726b1b08c426fd39cdaf
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-08-20 02:04:47 +0900

    change guest controller methods using repos

--- src/controller/guest.py
+++ src/controller/guest.py
@@ -1,52 +1,41 @@
-import logging
-import psycopg2 as ps
-
 from src.database.database import BaseDatabase
 from src.model.flat import Flat
 from src.model.tenant import Tenant
 from src.model.landlord import Landlord
+from src.repository.tenant import TenantRepository
+from src.repository.landlord import LandlordRepository
+from src.repository.flat import FlatRepository
+from src.repository.neighborhood import NeighborhoodRepository
+from src.repository.goods import GoodsRepository
 
 
 class GuestController:
     def __init__(self, db: BaseDatabase):
-        self._db = db
+        self._tenant_repo = TenantRepository(db)
+        self._landlord_repo = LandlordRepository(db)
+        self._flat_repo = FlatRepository(db)
+        self._neighborhood_repo = NeighborhoodRepository(db)
+        self._goods_repo = GoodsRepository(db)
 
-    def check_tenant(self, tenant_id: int):
-        return self._db.check_tenant(tenant_id)
+    def check_tenant(self, tenant_id: int) -> bool:
+        return self._tenant_repo.check_tenant(tenant_id)
 
-    def register_tenant(self, user_id: int, full_name: str, sex: str, city: str,
-                        qualities: str, age: int, solvency: bool):
-        try:
-            tenant = self._db.add_tenant(user_id, full_name, sex, city, qualities, age, solvency)
-            return Tenant(*tenant)
-        except ps.errors.CheckViolation as e:
-            logging.error(e)
-            logging.error(f'Invalid params while registration tenant with name \'{full_name}\'')
-        return Tenant()
+    def register_tenant(self, tenant: Tenant) -> Tenant:
+        tenant = self._tenant_repo.add_tenant(tenant)
+        return tenant
 
-    def check_landlord(self, landlord_id: int):
-        return self._db.check_landlord(landlord_id)
+    def check_landlord(self, landlord_id: int) -> bool:
+        return self._landlord_repo.check_landlord(landlord_id)
 
-    def register_landlord(self, user_id: int, full_name: str, city: str, age: int, phone: str, username: str):
-        rating = 0.0
-        try:
-            landlord = self._db.add_landlord(user_id, full_name, city, rating, age, phone, username)
-            return Landlord(*landlord)
-        except ps.errors.CheckViolation as e:
-            logging.error(e)
-            logging.error(f'Invalid params while registration landlord with name \'{full_name}\'')
-        return Landlord()
+    def register_landlord(self, landlord: Landlord) -> Landlord:
+        landlord = self._landlord_repo.add_landlord(landlord)
+        return landlord
 
-    def get_flats(self):
-        flats = [Flat(*flat) for flat in self._db.get_flats()]
-        photos = [self._db.get_flat_photos(flat.flat_id) for flat in flats]
+    def get_flats(self) -> tuple[list[Flat], list[list[str]]]:
+        flats = self._flat_repo.get_flats()
+        photos = [self._flat_repo.get_photos(flat.id) for flat in flats]
         return flats, photos
 
-    def get_landlord(self, landlord_id: int):
-        landlord = Landlord(*self._db.get_landlord(landlord_id))
-        if landlord:
-            logging.info(f'Get landlord by id \'{landlord_id}\'')
-            return landlord
-        else:
-            logging.error(f'Unable to get landlord by id = \'{landlord_id}\'')
-            return None
+    def get_landlord(self, landlord_id: int) -> Landlord:
+        landlord = self._landlord_repo.get_landlord(landlord_id)
+        return landlord

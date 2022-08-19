a8c482dbb70f1714f31d6f69bd2c99466bc1794e
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-08-20 03:00:01 +0900

    small fixes

--- src/controller/tenant.py
+++ src/controller/tenant.py
@@ -3,13 +3,13 @@ from src.controller.guest import GuestController
 
 class TenantController(GuestController):
     def check_tenant(self, tenant_id: int):
-        return self._db.check_tenant(tenant_id)
+        return self._tenant_repo.check_tenant(tenant_id)
 
     def check_landlord(self, landlord_id: int):
-        return self._db.check_landlord(landlord_id)
+        return self._landlord_repo.check_landlord(landlord_id)
 
     def register_tenant(self, *args):
-        raise AttributeError('\'B\' object has no attribute \'register_tenant\'')
+        raise AttributeError('\'TenantController\' object has no attribute \'register_tenant\'')
 
     def register_landlord(self, *args):
-        raise AttributeError('\'B\' object has no attribute \'register_landlord\'')
+        raise AttributeError('\'TenantController\' object has no attribute \'register_landlord\'')

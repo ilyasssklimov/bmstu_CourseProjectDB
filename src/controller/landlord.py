b0f70473422144671eaae87f704b86d7f527c50a
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-08-22 18:20:28 +0900

    add repos to landlord controller

--- src/controller/landlord.py
+++ src/controller/landlord.py
@@ -1,26 +1,12 @@
-import logging
-import psycopg2 as ps
-
 from src.controller.tenant import TenantController
 from src.model.flat import Flat
 
 
 class LandlordController(TenantController):
-    def add_flat(self, owner_id: int, price: int, rooms: int, square: float, address: str, metro: str,
-                 floor: int, max_floor: int, description: int):
-        try:
-            flat = self._db.add_flat(owner_id, price, rooms, square, address, metro, floor, max_floor, description)
-            return Flat(*flat)
-        except ps.errors.CheckViolation as e:
-            logging.error(e)
-            logging.error(f'Invalid params while adding flat with owner_id \'{owner_id}\'')
-        return Flat()
+    def add_flat(self, flat: Flat) -> Flat:
+        new_flat = self._flat_repo.add_flat(flat)
+        return new_flat
 
-    def add_photo(self, flat_id: int, photo: str):
-        try:
-            get_photo = self._db.add_photo(flat_id, photo)
-            return get_photo
-        except ps.errors.CheckViolation as e:
-            logging.error(e)
-            logging.error(f'Invalid params while adding photo to flat with id \'{flat_id}\'')
-        return ''
+    def add_photo(self, flat_id: int, photo: str) -> str:
+        photo = self._flat_repo.add_photo(flat_id, photo)
+        return photo
